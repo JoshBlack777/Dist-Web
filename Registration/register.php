75f0e494ff2164299ca56e440538efe89963b5c8
JamesBNguyen <47697053+JamesBNguyen@users.noreply.github.com>
2019-11-13 15:12:56 -0500

    Add files via upload

---
+++ Registration/register.php
@@ -0,0 +1,41 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="header">
+		<h2>Register</h2>
+	</div>
+	
+	<form method="post" action="register.php">
+
+		<?php include('errors.php'); ?>
+
+		<div class="input-group">
+			<label>Username</label>
+			<input type="text" name="username">
+		</div>
+		<div class="input-group">
+			<label>Email</label>
+			<input type="email" name="email">
+		</div>
+		<div class="input-group">
+			<label>Password</label>
+			<input type="password" name="password_1">
+		</div>
+		<div class="input-group">
+			<label>Confirm Password</label>
+			<input type="password" name="password_2">
+		</div>
+		<div class="input-group">
+			<button type="submit" class="btn" name="reg_user">Register</button>
+		</div>
+		<p>
+			<a href="login.php">Sign in</a>
+		</p>
+	</form>
+</body>
+</html>
